220e010d666b8a01d3073d2dadd52252e5f4c563
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2023-01-24 00:22:24 +0300

    added draw state

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import TicTacToeTwoPlayer from './src/TicTacToeTwoPlayer';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1}}>
+      <TicTacToeTwoPlayer/>
     </View>
   );
 }
